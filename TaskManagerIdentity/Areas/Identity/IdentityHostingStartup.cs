588ed02ad234415896e3e7ca2984d2a6b00224a6
Nur Furqon <nurfurqon2425@outlook.com>
2023-04-14 06:14:00 +0700

    Identity Login

---
+++ TaskManagerIdentity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TaskManagerIdentity.Data;
+
+[assembly: HostingStartup(typeof(TaskManagerIdentity.Areas.Identity.IdentityHostingStartup))]
+namespace TaskManagerIdentity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
