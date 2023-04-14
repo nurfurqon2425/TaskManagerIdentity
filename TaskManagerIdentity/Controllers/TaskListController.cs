ebc9aac01653b5bf74d2217608dddd83637a9d4f
Nur Furqon <nurfurqon2425@outlook.com>
2023-04-14 17:12:40 +0700

    Add Basic View

---
+++ TaskManagerIdentity/Controllers/TaskListController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManagerIdentity.Controllers
+{
+    public class TaskListController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ TaskManagerIdentity/Models/TaskList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManagerIdentity.Models
+{
+    public class TaskList
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string TaskName { get; set; }
+        [Required]
+        [Range(1, 5, ErrorMessage = "Priority only 1 until 5")]
+        public int Priority { get; set; }
+        [Required]
+        public bool Active { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+    }
+}
