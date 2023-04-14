a23b42109f9957bbda4fa5a15a7d4380af734995
Nur Furqon <nurfurqon2425@outlook.com>
2023-04-14 22:50:02 +0700

    Create Page and List Page with Search and Paging plus fontawesome

--- TaskManagerIdentity/Controllers/TaskListController.cs
+++ TaskManagerIdentity/Controllers/TaskListController.cs
@@ -19,17 +19,57 @@ namespace TaskManagerIdentity.Controllers
         }
 
         [Authorize]
-        public IActionResult List()
+        public IActionResult List(string SearchText = "", int pg = 1)
         {
-            IEnumerable<TaskList> objList = _db.TaskList;
+            IEnumerable<TaskList> objList;
+
+            if(SearchText != "" && SearchText != null)
+            {
+                objList = _db.TaskList.Where(p => p.TaskName.Contains(SearchText));
+            }
+            else
+            {
+                objList = _db.TaskList;
+            }
+
+            const int pageSize = 5;
+            if (pg < 1)
+                pg = 1;
+
+            int recsCount = objList.Count();            
+
+            int recSkip = (pg - 1) * pageSize;
+
+            IEnumerable<TaskList> retTaskList = objList.Skip(recSkip).Take(pageSize);
+
+            Pager pager = new Pager(recsCount, pg, pageSize);
+
+            this.ViewBag.Pager = pager;
+
             return View(objList);
         }
 
+        //GET - CREATE
         [Authorize]
         public IActionResult Create()
         {
             return View();
         }
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(TaskList obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.TaskList.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return View(obj);
+        }
+
         [Authorize]
         public IActionResult Update()
         {
---
+++ TaskManagerIdentity/Models/Pager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManagerIdentity.Models
+{
+    public class Pager
+    {
+        public string SearchText { get; set; }
+        public int TotalItems { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int StartPage { get; set; }
+        public int EndPage { get; set; }
+
+
+        public Pager()
+        {
+
+        }
+
+        public Pager(int totalItems, int page, int pageSize = 5)
+        {
+            int totalPages = (int)Math.Ceiling((decimal)totalItems / (decimal)pageSize);
+            int currentPage = page;
+
+            int startPage = currentPage - 2;
+            int endPage = currentPage + 2;
+
+            if(startPage <= 0)
+            {
+                endPage = endPage - (startPage - 1);
+                startPage = 1;
+            }
+
+            if(endPage > totalPages)
+            {
+                endPage = totalPages;
+                if(endPage > 5)
+                {
+                    startPage = endPage - 4;
+                }
+            }
+
+            TotalItems = totalItems;
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            TotalPages = totalPages;
+            StartPage = startPage;
+            EndPage = endPage;
+
+        }
+
+
+    }
+}
